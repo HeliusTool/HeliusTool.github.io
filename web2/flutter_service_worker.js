d1cdd3421abf2c1af3738b656a75ba4dfb2826f0
LucasHLeal <lucas.hanauer@outlook.com>
2021-09-27 17:12:57 -0300

    Filtro de tecnicas

--- web2/flutter_service_worker.js
+++ web2/flutter_service_worker.js
@@ -39,9 +39,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "2e1125725eacdf932e70b43000d850ae",
-"/": "2e1125725eacdf932e70b43000d850ae",
-"main.dart.js": "e3263782b536769e964291a16b1da8dd",
+"index.html": "d04c39c2623e10d6e90d6b928532ff56",
+"/": "d04c39c2623e10d6e90d6b928532ff56",
+"main.dart.js": "ba19a217af28a37e11ccd3c5004a1e0f",
 "manifest.json": "4b5df4933d31a559d4d5f22ef45c53b8",
 "version.json": "66663c5211306423f9ed23f3d3ca2a6e"
 };
