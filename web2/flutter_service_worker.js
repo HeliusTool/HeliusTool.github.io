86e0e0d839bd354a20f298504357b165e687b00f
LucasHLeal <lucas.hanauer@outlook.com>
2021-12-07 20:37:47 -0300

    Novos opções de recomendação

--- web2/flutter_service_worker.js
+++ web2/flutter_service_worker.js
@@ -71,9 +71,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "7bb0769441a9036e684ab585b8f65929",
-"/": "7bb0769441a9036e684ab585b8f65929",
-"main.dart.js": "1d01470155af0d27f205e356a1367d35",
+"index.html": "2b4090d4c4ade7580f29311d87c80824",
+"/": "2b4090d4c4ade7580f29311d87c80824",
+"main.dart.js": "400d5427ed55a2d1003e980ca8ec9659",
 "manifest.json": "4b5df4933d31a559d4d5f22ef45c53b8",
 "version.json": "66663c5211306423f9ed23f3d3ca2a6e"
 };
